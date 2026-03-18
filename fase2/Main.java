d30bb6e7ff7f22032a0ab4ffd5126ed8d601f60f
paulo <paulo.sergio2001jr@gmail.com>
2026-03-18 18:57:42 -0300

    Fase 2 - Adiciona Scanner no cadastro de carros

--- fase2/Main.java
+++ fase2/Main.java
@@ -1,22 +1,38 @@
 package fase2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
         Carro carro1 = new Carro();
 
-        carro1.marca = "chevrolet";
-        carro1.modelo = "corsa classic";
-        carro1.ano = 2010;
+        System.out.println("=== Cadastro de Veiculo 1 ===");
+
+        System.out.println("Digite a marca:");
+        carro1.marca = leitor.nextLine();
+        System.out.println("Digite o modelo:");
+        carro1.modelo = leitor.nextLine();
+        System.out.println("Digite o ano:");
+        carro1.ano = leitor.nextInt();
+        leitor.nextLine();
 
         carro1.descricao();
 
         Carro carro2 = new Carro();
 
-        carro2.marca = "renault";
-        carro2.modelo = "fluence";
-        carro2.ano = 2014;
+        System.out.println("=== Cadastro de Veiculo 2 ===");
+
+        System.out.println("Digite a marca:");
+        carro2.marca = leitor.nextLine();
+        System.out.println("Digite o modelo:");
+        carro2.modelo = leitor.nextLine();
+        System.out.println("Digite o ano:");
+        carro2.ano = leitor.nextInt();
+        leitor.nextLine();
 
         carro2.descricao();
+        leitor.close();
 
     }
 
