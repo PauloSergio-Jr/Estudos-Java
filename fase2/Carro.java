ef566125a207f416c39bbf318cb342ce52ae81fc
paulo <paulo.sergio2001jr@gmail.com>
2026-03-18 20:22:14 -0300

    Fase 2 - Encapsulamento com getters e setters

--- fase2/Carro.java
+++ fase2/Carro.java
@@ -1,14 +1,43 @@
 package fase2;
 
 public class Carro {
-    String marca;
-    String modelo;
-    int ano;
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        if (ano >= 1886) {
+            this.ano = ano;
+        } else {
+            System.out.println("Erro, o ano do modelo não pode ser anterior a 1886!");
+        }
+    }
 
     public Carro(String marca, String modelo, int ano) {
         this.marca = marca;
         this.modelo = modelo;
-        this.ano = ano;
+        setAno(ano);
+
     }
 
     void descricao() {
